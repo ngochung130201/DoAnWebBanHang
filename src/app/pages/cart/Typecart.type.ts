291036d4fca27fa7a92fd5adeed40f1dc21dac62
HuynhNgocHung <huynhngochungIT@gmail.com.vn>
2023-01-11 15:57:22 +0700

    cart chua xong

---
+++ src/app/pages/cart/Typecart.type.ts
@@ -0,0 +1,13 @@
+type TyCart = {
+  id: string,
+  name: string,
+  image: string,
+  amount: number,
+  priceDiscount: number,
+  price: number,
+  intoMoney: number,
+  sumMoney: number
+}
+export {
+  TyCart
+}
